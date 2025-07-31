d4491a225130e6ecf17c0ea3ce6acc430b57e4d0
Devyani Ayare <ayaredevyani@gmail.com>
2025-07-31 13:21:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!"); //print 
+        System.out.ntln("Hello, Java!"); //print 
     }
 }
